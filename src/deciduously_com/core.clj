f3554782c8bb1bdd0c7b1e86666c23bb4699a83e
Ben Lovy <deciduously@users.noreply.github.com>
2018-01-09 09:45:57 -0500

    Remove dev build option from jar

--- src/deciduously_com/core.clj
+++ src/deciduously_com/core.clj
@@ -1,12 +1,10 @@
 (ns deciduously-com.core
-  (:require [deciduously-com.web :refer [export prod-handler dev-handler]]
+  (:require [deciduously-com.web :refer [export prod-handler]]
             [ring.adapter.jetty :refer [run-jetty]])
   (:gen-class))
 
 (def port (Integer/valueOf (or (System/getenv "PORT") "3000")))
 
 (defn -main [& args]
-  (let [mode (System/getenv "BUILD")]
-    (if (= mode "prod")
-      (do (export) (run-jetty prod-handler {:port port}))
-      (run-jetty dev-handler {:port port}))))
+  (export)
+  (run-jetty prod-handler {:port port}))
