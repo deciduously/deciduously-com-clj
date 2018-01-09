0a804358c0109ac2647804b4824de8d5846df8a9
Ben Lovy <deciduously@users.noreply.github.com>
2018-01-09 11:04:08 -0500

    grab env from web ns

--- src/deciduously_com/core.clj
+++ src/deciduously_com/core.clj
@@ -1,10 +1,7 @@
 (ns deciduously-com.core
-  (:require [deciduously-com.web :refer [export prod-handler]]
+  (:require [deciduously-com.web :refer [export port prod-handler target-dir]]
             [ring.adapter.jetty :refer [run-jetty]])
   (:gen-class))
-
-(def port (Integer/valueOf (or (System/getenv "PORT") "3000")))
-(def target-dir (or (System/getenv "DIST") "dist/"))
 
 (defn -main [& args]
   (export target-dir)
