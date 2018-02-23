fd20b7b861f5c52a07a06f8bc20bc3bb1177ae57
deciduously <github@deciduously.com>
2018-02-22 20:35:38 -0500

    Docstrings

--- src/deciduously_com/highlight.clj
+++ src/deciduously_com/highlight.clj
@@ -2,15 +2,21 @@
   (:require [clygments.core :as clygments]
             [net.cgrand.enlive-html :as enlive]))
 
-(defn- extract-code [highlighted]
+(defn- extract-code
+  "Use Enlive to grab the code element to highlight"
+  [highlighted]
   (-> highlighted java.io.StringReader. enlive/html-resource (enlive/select [:pre]) first :content))
 
-(defn- highlight [node]
+(defn- highlight
+  "Apply proper class for styling to node"
+  [node]
   (let [code (->> node :content (apply str))
         lang (->> node :attrs :class keyword)]
     (assoc node :content (-> code (clygments/highlight lang :html) extract-code))))
 
-(defn highlight-code-blocks [page]
+(defn highlight-code-blocks
+  "Run highlighter on each code block"
+  [page]
   (enlive/sniptest page
                    [:pre :code] highlight
                    [:pre :code] #(assoc-in % [:attrs :class] "highlight")))
